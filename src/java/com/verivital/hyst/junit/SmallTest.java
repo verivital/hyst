d075eb92fbe3dac325552500f8672edced1b39af
Stanley Bak <bak2007@gmail.com>
2015-09-17 16:01:55 -0400

    moved single test in intRep to smalltest

--- src/java/com/verivital/hyst/junit/SmallTest.java
+++ src/java/com/verivital/hyst/junit/SmallTest.java
@@ -35,6 +35,11 @@ import com.verivital.hyst.util.RangeExtractor;
 import com.verivital.hyst.util.RangeExtractor.ConstantMismatchException;
 import com.verivital.hyst.util.RangeExtractor.EmptyRangeException;
 
+import de.uni_freiburg.informatik.swt.sxhybridautomaton.Bind;
+import de.uni_freiburg.informatik.swt.sxhybridautomaton.ParamMap;
+import de.uni_freiburg.informatik.swt.sxhybridautomaton.SpaceExDocument;
+import de.uni_freiburg.informatik.swt.sxhybridautomaton.SpaceExNetworkComponent;
+
 /**
  * Small tests dealing with the expression parser or range extractor. Things that don't require
  * loading a whole model.
@@ -778,6 +783,36 @@ public class SmallTest
 			System.out.println();
 		}*/
 	}
+	
+	@Test
+	/**
+	 * Ensure a bind cannot have multiple parameters added with the same names 
+	 * (SpaceEx will not accept these if done and had an error where a model was created with multiple binds)
+	 */
+    public void testSpaceExNetworkComponentDuplicateBind() 
+	{
+        // Network component
+        SpaceExDocument sed = new SpaceExDocument();
+        SpaceExNetworkComponent net = new SpaceExNetworkComponent(sed);
+        Bind bind = new Bind(net);
+        
+        ParamMap varMapA = new ParamMap(bind, "test_var", "test_var");
+        
+        // TODO: faked passing test for now, uncomment will cause it to fail, need to fix this in binds to check
+        // if param with duplicate name exists or not
+        //
+        //ParamMap varMapB = new ParamMap(bind, "test_var", "test_var");
+        
+        boolean duplicate = false;
+        for (int i = 0; i < bind.getMapCount(); i++) {
+        	for (int j = 0; j < bind.getMapCount(); j++) {
+        		if (i != j) {
+	        		//if (bind.getMap(i).getKey() == bind.getMap(j).getKey())
+	        		Assert.assertNotEquals(bind.getMap(i).getKey(), bind.getMap(j).getKey());
+        		}
+        	}
+        }
+    }
 	
 	/**
 	 * Test using LUT in flow
