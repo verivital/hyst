5592c46b2525f470eb13607d54abb80e5f89dba0
Stanley Bak <stanleybak@gmail.com>
2016-02-12 15:10:56 -0500

    added file

---
+++ src/java/com/verivital/hyst/geometry/SymbolicStatePoint.java
@@ -0,0 +1,15 @@
+package com.verivital.hyst.geometry;
+
+public class SymbolicStatePoint
+{
+	public SymbolicStatePoint() {}
+	
+	public SymbolicStatePoint(String mode, HyperPoint pt)
+	{
+		this.modeName = mode;
+		this.hp = pt;
+	}
+	
+	public String modeName;
+	public HyperPoint hp;
+}
