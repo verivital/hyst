6d6c9ce097f3a2d794f3e3c72768e0ff05b456bd
Stanley Bak <stanleybak@gmail.com>
2016-02-29 16:59:57 -0500

    added pythonbridge tests to standard python unit tests in build.xml

--- src/pythonbridge/test_interval_optimize.py
+++ src/pythonbridge/test_interval_optimize.py
@@ -17,12 +17,28 @@ class TestIntervalOptimize(unittest.TestCase):
         #eq = 1*x*x + (0.2-x) / x + sin(x+0.01) + sqrt(x + 1) + cos(x + 0.01) + tan(x + 0.01) - (x+0.1)**(x+0.1)
 
         result = opt.eval_eq(eq, {'x':interval(0.20, 0.21)})
-        print "result = " + str(result)
 
-        expected = [math.sin(0.211), math.sin(0.22)]
-        print "expected = " + str(result)
-        self.assertAlmostEquals(result, expected)
+        expected = [math.sin(0.21), math.sin(0.22)]
 
+        self.assertAlmostEquals(result.a, expected[0])
+        self.assertAlmostEquals(result.b, expected[1])
+
+    def test_eval_eq_multi(self):
+        '''test for the eval_eq_multi function'''
+        x = symbols('x')
+        eq1 = x + interval(0.1)
+        eq2 = x + interval(0.2)
+        range1 = {'x':interval(0, 1)}
+        range2 = {'x':interval(1, 2)}
+
+        # todo: this test currently fails because _multi was only coded
+        # for a SINGLE equation, not multiple ones, update it!!!!
+        res = opt.eval_eq_multi([eq1, eq2], [range1, range2])
+
+        self.assertAlmostEquals(res[0].a, 0.1)
+        self.assertAlmostEquals(res[0].b, 1.1)
+        self.assertAlmostEquals(res[1].a, 1.2)
+        self.assertAlmostEquals(res[1].b, 2.2)
 
 if __name__ == '__main__':
     unittest.main()
