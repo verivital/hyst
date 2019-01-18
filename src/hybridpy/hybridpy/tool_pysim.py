c793737341ccb3958e4414eabc29283e50bee7d5
Max Gaukler <max.gaukler@fau.de>
2019-01-18 13:53:14 +0100

    Bugfix: Hypy pysim did not return output
    Calling pysim via hypy.Engine('pysim') did not return the output result even if parse_output is True.
    
    The current fix is slightly sub-optimal because the output is passed via a temporary file which is always deleted, even if you specify an explicit temporary directory.

--- src/hybridpy/hybridpy/tool_pysim.py
+++ src/hybridpy/hybridpy/tool_pysim.py
@@ -3,6 +3,8 @@
 import imp
 import os
 import sys
+import pickle
+import tempfile
 
 from hybridpy.hybrid_tool import HybridTool
 from hybridpy.hybrid_tool import RunCode
@@ -40,7 +42,11 @@ class PySimTool(HybridTool):
 
         self._init_states = define_init_states(define_ha())
         self._result = sim(self._init_states, self._settings)
-
+        # We need to pass the result via stdout (see note in parse_output()).
+        # To do that, we serialize and write it to a file and print the filename.
+        with tempfile.NamedTemporaryFile(prefix='pysim_result',suffix='.pickle', delete=False) as filehandle:
+            pickle.dump(self._result, filehandle)
+            print "PYSIM_RESULT_FILE=" + filehandle.name
         return rv
 
     def _make_image(self):
@@ -54,12 +60,26 @@ class PySimTool(HybridTool):
 
         return True
 
-    def parse_output(self, dummy_directory, dummy_lines, dummy_hypy_out):
+    def parse_output(self, dummy_directory, lines, dummy_hypy_out):
         '''returns the parsed output object
 
         For pysim, this is the result of running simulate()
         '''
-
+        # NOTE: This function is not called on the same tool object as _run_tool and _make_image because these are run in a subprocess,
+        # whereas parse_output is run in the main python process that called hypy.Engine.run().
+        # Additionally, the temporary directory gets deleted before parse_output is called.
+        # Therefore, the data must be passed via stdout.
+        
+        prefix = "PYSIM_RESULT_FILE="
+        for line in lines:
+            if line.startswith(prefix):
+                # import data printed from _run_tool()
+                path = line[len(prefix):]
+                with open(path, 'rb') as f:
+                    self._result = pickle.load(f)
+                os.remove(path)
+            
+        assert self._result is not None, "could not find {} in output".format(prefix)
         return self._result
                 
 if __name__ == "__main__":
