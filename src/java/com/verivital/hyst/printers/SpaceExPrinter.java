verivital/hyst

commit 1591f8393113efb2d29c31a1da63bcada525d151

updated spaceex and hypy for atva heli measurements

src/java/com/verivital/hyst/printers/SpaceExPrinter.java
@@ -66,7 +66,7 @@ public class SpaceExPrinter extends ToolPrinter
 	@Option(name = "-scenario", usage = "spaceex solver scenario", metaVar = "VAL")
 	public String scenario = "auto";
 
-	@Option(name = "-output-format", usage = "spaceex output format", metaVar = "VAL")
+	@Option(name = "-output-format", usage = "spaceex output format (none = skip)", metaVar = "VAL")
 	String outputFormat = "auto";
 
 	@Option(name = "-iter-max", usage = "maximum number of jumps", metaVar = "VAL")
@@ -76,7 +76,10 @@ public class SpaceExPrinter extends ToolPrinter
 	String directions = "auto";
 
 	@Option(name = "-aggregation", usage = "aggregation parameter", metaVar = "VAL")
-	String aggregation = "auto";
+	String aggregation = "chull";
+
+	@Option(name = "-forbidden", usage = "forbidden parameter", metaVar = "VAL")
+	String forbidden = "none";
 
 	@Option(name = "-flowpipe_tol", usage = "flowpipe-tolerance parameter (0 = skip)", metaVar = "VAL")
 	String flowpipeTol = "auto";
@@ -218,6 +221,9 @@ public class SpaceExPrinter extends ToolPrinter
 
 		String agg = getParam(aggregation, config.settings.spaceExConfig.aggregation);
 		sed.setAggregation(agg);
+
+		String f = getParam(forbidden, config.settings.spaceExConfig.forbidden);
+		sed.setForbiddenOverride(f);
 
 		String scenario = getParam(this.scenario, config.settings.spaceExConfig.scenario);
 		sed.setScenario(scenario);

src/java/de/uni_freiburg/informatik/swt/spaceexxmlprinter/SpaceExXMLPrinter.java
@@ -656,7 +656,9 @@ public class SpaceExXMLPrinter
 		appendCfgString(rv, "system", config.systemID);
 		appendCfgString(rv, "scenario", config.scenario); // was supp
 		appendCfgString(rv, "directions", config.directions);
-		appendCfgString(rv, "set-aggregation", config.aggregation);
+
+		if (!config.aggregation.toLowerCase().equals("none"))
+			appendCfgString(rv, "set-aggregation", config.aggregation);
 
 		if (config.flowpipeTol > 0)
 			appendCfgString(rv, "flowpipe-tolerance", Double.toString(config.flowpipeTol));
@@ -664,7 +666,9 @@ public class SpaceExXMLPrinter
 		appendCfgString(rv, "sampling-time", Double.toString(config.samplingTime).toString());
 		appendCfgString(rv, "time-horizon", Double.toString(config.timeHorizon));
 		appendCfgString(rv, "iter-max", Integer.toString(config.maxIterations));
-		appendCfgString(rv, "output-format", config.outputFormat);
+
+		if (!config.outputFormat.toLowerCase().equals("none"))
+			appendCfgString(rv, "output-format", config.outputFormat);
 
 		if (!skipTol)
 		{
@@ -679,10 +683,18 @@ public class SpaceExXMLPrinter
 		if (e != null)
 			appendCfgString(rv, "initially",
 					"\"" + expressionToString(e, FormulaType.DEFAULT, "initial states") + "\"");
-		e = mSXDocument.getForbiddenStateConditions();
-		if (e != null)
-			appendCfgString(rv, "forbidden",
-					"\"" + expressionToString(e, FormulaType.DEFAULT, "forbidden states") + "\"");
+
+		if (!config.forbidden.toLowerCase().equals("none"))
+		{
+			appendCfgString(rv, "forbidden", "\"" + config.forbidden + "\"");
+		}
+		else
+		{
+			e = mSXDocument.getForbiddenStateConditions();
+			if (e != null)
+				appendCfgString(rv, "forbidden", "\""
+						+ expressionToString(e, FormulaType.DEFAULT, "forbidden states") + "\"");
+		}
 
 		// output-variables
 

src/.settings/org.eclipse.jdt.core.prefs
@@ -1,4 +1,15 @@
 eclipse.preferences.version=1
+org.eclipse.jdt.core.compiler.codegen.inlineJsrBytecode=enabled
+org.eclipse.jdt.core.compiler.codegen.methodParameters=do not generate
+org.eclipse.jdt.core.compiler.codegen.targetPlatform=1.7
+org.eclipse.jdt.core.compiler.codegen.unusedLocal=preserve
+org.eclipse.jdt.core.compiler.compliance=1.7
+org.eclipse.jdt.core.compiler.debug.lineNumber=generate
+org.eclipse.jdt.core.compiler.debug.localVariable=generate
+org.eclipse.jdt.core.compiler.debug.sourceFile=generate
+org.eclipse.jdt.core.compiler.problem.assertIdentifier=error
+org.eclipse.jdt.core.compiler.problem.enumIdentifier=error
+org.eclipse.jdt.core.compiler.source=1.7
 org.eclipse.jdt.core.formatter.align_type_members_on_columns=false
 org.eclipse.jdt.core.formatter.alignment_for_arguments_in_allocation_expression=16
 org.eclipse.jdt.core.formatter.alignment_for_arguments_in_annotation=0

src/hybridpy/hybridpy/hybrid_tool.py
@@ -178,7 +178,7 @@ def get_tool_path(filename, print_errors=True):
 
     returns the full path to the tool, or None 
     '''
-    
+
     rv = None
     errors = []
 
@@ -198,7 +198,7 @@ def get_tool_path(filename, print_errors=True):
 
                 try_path = os.path.join(dir_name, filename)
 
-                if os.path.exists(try_path):
+                if os.path.exists(try_path) and os.path.isfile(try_path):
                     rv = try_path
                     break
 

src/java/de/uni_freiburg/informatik/swt/sxhybridautomaton/SpaceExConfigValues.java
@@ -14,6 +14,7 @@ public class SpaceExConfigValues
 	public String directions = "oct";
 	public String aggregation = "chull";
 	public double flowpipeTol = 0.01;
+	public String forbidden = "none";
 	public boolean timeTriggered = false; // time-triggered support using
 											// "--map-zero-duration-jump-sets
 											// true"

src/java/de/uni_freiburg/informatik/swt/sxhybridautomaton/SpaceExDocument.java
@@ -166,6 +166,11 @@ public class SpaceExDocument
 		mConfig.aggregation = agg;
 	}
 
+	public void setForbiddenOverride(String f)
+	{
+		mConfig.forbidden = f;
+	}
+
 	public void setFlowpipeTolerance(double tol)
 	{
 		mConfig.flowpipeTol = tol;

commit a3b575782cc0e89a0ea2c7a4934e07fc86318b69

fix SpaceEx set-aggregation parameter
The setting set-aggregation=<value> from the .cfg file was ignored and changed to set-aggregation=chull, which yields different results.

Regression from 1591f8393113efb2d29c31a1da63bcada525d151.

src/java/com/verivital/hyst/printers/SpaceExPrinter.java
@@ -57,6 +57,7 @@ import de.uni_freiburg.informatik.swt.sxhybridautomaton.VariableParam;
  */
 public class SpaceExPrinter extends ToolPrinter
 {
+	// NOTE: "auto" is a magic constant that means "unset, fall back to default". See convert().
 	@Option(name = "-time", usage = "reachability time", metaVar = "VAL")
 	String time = "auto";
 
@@ -76,7 +77,7 @@ public class SpaceExPrinter extends ToolPrinter
 	String directions = "auto";
 
 	@Option(name = "-aggregation", usage = "aggregation parameter", metaVar = "VAL")
-	String aggregation = "chull";
+	String aggregation = "auto";
 
 	@Option(name = "-forbidden", usage = "forbidden parameter", metaVar = "VAL")
 	String forbidden = "none";

src/java/de/uni_freiburg/informatik/swt/spaceexxmlprinter/SpaceExXMLPrinter.java
@@ -656,9 +656,7 @@ public class SpaceExXMLPrinter
 		appendCfgString(rv, "system", config.systemID);
 		appendCfgString(rv, "scenario", config.scenario); // was supp
 		appendCfgString(rv, "directions", config.directions);
-
-		if (!config.aggregation.toLowerCase().equals("none"))
-			appendCfgString(rv, "set-aggregation", config.aggregation);
+		appendCfgString(rv, "set-aggregation", config.aggregation);
 
 		if (config.flowpipeTol > 0)
 			appendCfgString(rv, "flowpipe-tolerance", Double.toString(config.flowpipeTol));